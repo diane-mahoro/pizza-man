5f483de13c97f11df91bbd8e63963a33d2e0e621
diane-mahoro <www.diammah7@gmail.com>
2019-08-01 12:20:37 +0200

    another file

--- js/index.js
+++ js/index.js
@@ -1,3 +1,24 @@
 $(document).ready(function() {
+var Pizza = function(size,topping,crust,flavour){
+    this.size = size;
+    this.topping = topping;
+    this.crust = crust;
+    this.flavour = flavour;
+}
+$("form#form").submit(function(event){
+    event.preventDefault();
+    var s = $("#size").val();
+    var f =$("#flavour").val();
+    var c =$("#crust").val();
+    var t =$("#top").val();
+    var newPizza = new Pizza(s,f,c,t);
+   
 
+
+
+    $("#size").val("");
+    $("#flavour").val("");
+    $("#crust").val("");
+    $("#top").val("");
+});
 });
