c054c2725e0e969b6c97119c2d364be02417ac9d
diane-mahoro <www.diammah7@gmail.com>
2019-08-01 12:55:39 +0200

    readme file

--- js/index.js
+++ js/index.js
@@ -7,15 +7,20 @@ var Pizza = function(size,topping,crust,flavour){
 }
 $("form#form").submit(function(event){
     event.preventDefault();
+    var p=0;
     var s = $("#size").val();
     var f =$("#flavour").val();
     var c =$("#crust").val();
     var t =$("#top").val();
     var newPizza = new Pizza(s,f,c,t);
-   
-
-
-
+    if(newPizza.s=="large" && newPizza.f =="Berry, Arugula and Prosciutto Pizza" && newPizza.c =="Thin" && newPizza.t.length =="Berry, Arugula and Prosciutto Pizza"){
+        p = 11000;
+    }
+    else if(newPizza.s=="medium" && newPizza.f =="Berry, Arugula and Prosciutto Pizza" && newPizza.c =="Thin" && newPizza.t.length =="Berry, Arugula and Prosciutto Pizza"){
+        p= 8000;
+    }
+    else{p=6000;}
+    return alert(p);
     $("#size").val("");
     $("#flavour").val("");
     $("#crust").val("");
