a33c50c0a764d6e4cb7e4b1463219543fbce1311
diane-mahoro <www.diammah7@gmail.com>
2019-08-05 09:49:08 +0200

    change in file

--- js/index.js
+++ js/index.js
@@ -5,6 +5,12 @@ var Pizza = function(size,topping,crust,flavour){
     this.crust = crust;
     this.flavour = flavour;
 }
+function resetFields() {
+    $("#size").val("");
+    $("#flavour").val("");
+    $("#crust").val("");
+    $("#top").val("");
+}
 $("button#another").click(function(event){
     event.preventDefault();
 $("#jessy").append(
@@ -17,31 +23,28 @@ $("#jessy").append(
    '<label for="top">Toppings</label> <br>' +
    '<input type="text" id="fourth1">'
 );
-$("div.col-md-4").append(
-   '<h4>ANOTHER PIZZA ORDER DETAIL:</h4>' +
-              '<P>Size: <span id="A">' + +'</span></P>' +
-              '<P>Flavour: <span id="B"></span></P>' +
-              '<P>Crust: <span id="C"></span></P>' +
-             '<P>Toppings: <span id="D"></span></P>' +
-              '<p>Total price: <span id="price1"></span></p>'
-);
+// $("div.col-md-4").append(
+//    '<h4>ANOTHER PIZZA ORDER DETAIL:</h4>' +
+//               '<P>Size: <span id="A">' + +'</span></P>' +
+//               '<P>Flavour: <span id="B"></span></P>' +
+//               '<P>Crust: <span id="C"></span></P>' +
+//              '<P>Toppings: <span id="D"></span></P>' +
+//               '<p>Total price: <span id="price1"></span></p>'
+// );
 });
 $("form#form").submit(function(event){
     event.preventDefault();
+    $(".col-md-4").show();
     var p=0;
-    var s = $("#first").val().toLowerCase();
-    var f =$("#second").val().toLowerCase();
-    var c =$("#third").val().toLowerCase();
-    var t =$("#fourth").val().toLowerCase();
+    var s = $("select.select1").val().toLowerCase();
+    var f =$("#select2").val().toLowerCase();
+    var c =$("#select3").val().toLowerCase();
+    var t =$("#select4").val().toLowerCase();
     var newPizza = new Pizza(s,f,c,t);
     $("span#first2").text(newPizza.size);
     $("span#second2").text(newPizza.flavour);
     $("span#third2").text(newPizza.crust);
     $("span#fourth2").text(newPizza.topping);
-    $("#size").val("");
-    $("#flavour").val("");
-    $("#crust").val("");
-    $("#top").val("");
         if( newPizza.size == 'large'){
             p = 15000;
             $("span#price").text(p + 'frw');
@@ -54,6 +57,7 @@ $("form#form").submit(function(event){
             p = 8000;
             $("span#price").text(p + 'frw');
         }
+        resetFields();
 });
 $("button#yes").click(function(){
     prompt("enter your address");
